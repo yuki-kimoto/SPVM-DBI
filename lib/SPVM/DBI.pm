54f138b8b7a8a52d1f23b1ad06701e450bf159e2
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-02-07 10:05:16 +0900

    Update the document and implementation.

--- lib/SPVM/DBI.pm
+++ lib/SPVM/DBI.pm
@@ -8,56 +8,71 @@ our $VERSION = "0.001";
 
 =head1 Name
 
-SPVM::DBI - Short Description
+SPVM::DBI - DataBase Interface for SPVM (PRE-ALPHA)
 
 =head1 Description
 
-DBI class in L<SPVM> has methods to do someting.
+L<SPVM::DBI> is a database interface for the SPVM programming language.
+
+B<WARNING: THIS MODULE IS UNDER HEAVY DEVELOPMENT AND IS NOT YET FUNCTIONAL.>
+
+This is a pre-alpha release. The following points should be noted:
+
+=over 2
+
+=item * This module does not work at all at this stage.
+
+=item * No tests have been written yet.
+
+=item * The public API is not even decided, so everything is private and subject to total change.
+
+=item * DO NOT USE this module. It is only a placeholder for future development.
+
+=back
 
 =head1 Usage
 
-  use DBI;
+    # No usage available yet.
 
 =head1 Details
 
-
+None.
 
 =head1 Super Class
 
-
+L<AnyObject|SPVM::AnyObject>
 
 =head1 Interfaces
 
-
+None.
 
 =head1 Enumerations
 
-
+None.
 
 =head1 Fields
 
-
+None.
 
 =head1 Class Methods
 
-
+None.
 
 =head1 Instance Methods
 
-
+None.
 
 =head1 Well Known Child Classes
 
-
+None.
 
 =head1 See Also
 
-
-
+L<DBI>
 
 =head1 Repository
 
-
+L<https://github.com/yuki-kimoto/SPVM-DBI>
 
 =head1 Author
 
@@ -69,3 +84,4 @@ Copyright (c) 2026 Yuki Kimoto
 
 MIT License
 
+=cut
