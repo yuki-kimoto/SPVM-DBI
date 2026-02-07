31357d9569fbd40ae23d9fafcd4083f0eef5bd57
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-02-07 13:43:24 +0900

    Add DBI::Error::SQLState class.

---
+++ lib/SPVM/DBI/Error/SQLState.pm
@@ -0,0 +1,71 @@
+package SPVM::DBI::Error::SQLState;
+
+
+
+1;
+
+=encoding utf8
+
+=head1 Name
+
+SPVM::DBI::Error::SQLState - Short Description
+
+=head1 Description
+
+DBI::Error::SQLState class in L<SPVM> has methods to do someting.
+
+=head1 Usage
+
+  use DBI::Error::SQLState;
+
+=head1 Details
+
+
+
+=head1 Super Class
+
+
+
+=head1 Interfaces
+
+
+
+=head1 Enumerations
+
+
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+=head1 Well Known Child Classes
+
+
+
+=head1 See Also
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2026 Yuki Kimoto
+
+MIT License
+
