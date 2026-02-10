30842bbd754ada86e9a508a169cbb54a84029d05
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-02-10 14:37:32 +0900

    Added the document for DBI.

--- lib/SPVM/DBI.pm
+++ lib/SPVM/DBI.pm
@@ -1,74 +1,105 @@
 package SPVM::DBI;
 
-our $VERSION = "0.001";
-
 1;
 
 =encoding utf8
 
 =head1 Name
 
-SPVM::DBI - DataBase Interface for SPVM (PRE-ALPHA)
+SPVM::DBI - Database Independent Interface
 
 =head1 Description
 
-L<SPVM::DBI> is a database interface for the SPVM programming language.
-
-B<WARNING: THIS MODULE IS UNDER HEAVY DEVELOPMENT AND IS NOT YET FUNCTIONAL.>
-
-This is a pre-alpha release. The following points should be noted:
-
-=over 2
-
-=item * This module does not work at all at this stage.
-
-=item * No tests have been written yet.
-
-=item * The public API is not even decided, so everything is private and subject to total change.
-
-=item * DO NOT USE this module. It is only a placeholder for future development.
-
-=back
+L<DBI|SPVM::DBI> class in L<SPVM> provides a database-independent interface for database connections. It acts as a factory to create a database handle (L<DBI::Db|SPVM::DBI::Db>) by loading the appropriate driver (L<DBI::Dr|SPVM::DBI::Dr>).
 
 =head1 Usage
 
-    # No usage available yet.
+  use DBI;
+  use DBD::SQLite;
+  use Go::Context;
+  
+  my $ctx = Go::Context->new;
+  my $dsn = "dbi:SQLite:dbname=:memory:";
+  
+  # 1. Connect to a database
+  my $dbh = DBI->connect($ctx, $dsn, "user", "password");
+  
+  # 2. Prepare a statement
+  my $sql = "SELECT id, name FROM users WHERE id > ?";
+  my $sth = $dbh->prepare($ctx, $sql);
+  
+  # 3. Execute with bind values (Box primitives using (object))
+  $sth->execute($ctx, [(object)10]);
+  
+  # 4. Fetch rows in a while loop
+  while (my $row = $sth->fetchrow_array($ctx)) {
+    my $id = $row->[0]->(int);
+    my $name = $row->[1]->(string);
+    
+    # Process the data...
+  }
 
-=head1 Details
+=head1 Class Variables
 
-None.
+=head2 $DRIVERS_H
 
-=head1 Super Class
+C<our $DRIVERS_H : cache L<Hash|SPVM::Hash> of L<DBI::Dr|SPVM::DBI::Dr>;>
 
-L<AnyObject|SPVM::AnyObject>
-
-=head1 Interfaces
-
-None.
-
-=head1 Enumerations
-
-None.
-
-=head1 Fields
-
-None.
+A cache for driver handles. The keys are driver names (e.g., "SQLite"), and the values are their corresponding driver handle objects (L<DBI::Dr|SPVM::DBI::Dr>). This cache is used by L</"connect"> to reuse existing driver instances.
 
 =head1 Class Methods
 
-None.
+=head2 connect
 
-=head1 Instance Methods
+C<static method connect : L<DBI::Db|SPVM::DBI::Db> ($ctx : L<Go::Context|SPVM::Go::Context>, $dsn : string, $user : string = undef, $password : string = undef, $options : object[] = undef)>
 
-None.
+Establishes a connection to the database specified by the C<$dsn>.
 
-=head1 Well Known Child Classes
+This method performs the following:
 
-None.
+=over 2
+
+=item 1.
+
+Parses the C<$dsn> to identify the driver name (e.g., "SQLite" in "dbi:SQLite:...").
+
+=item 2.
+
+Checks the internal driver cache L</"$DRIVERS_H">. If an instance of the driver already exists, it is reused.
+
+=item 3.
+
+If the driver is not cached, it loads the corresponding driver class (e.g., C<DBD::SQLite>), creates a new instance, and stores it in the L</"$DRIVERS_H"> cache for future use.
+
+=item 4.
+
+Calls the L<connect|SPVM::DBI::Dr/"connect"> method of the driver and returns a database handle (L<DBI::Db|SPVM::DBI::Db>).
+
+=back
+
+C<$options> is an array of key-value pairs. Supported options are defined in L<option_names|SPVM::DBI::Db/"option_names">.
+
+Exceptions:
+
+=over 2
+
+=item *
+
+If C<$dsn> is not defined.
+
+=item *
+
+If the DSN format is invalid.
+
+=item *
+
+If the driver class cannot be loaded or an error occurs during connection.
+
+=back
 
 =head1 See Also
 
-L<DBI>
+L<DBI::Db|SPVM::DBI::Db>, L<DBI::Dr|SPVM::DBI::Dr>, L<Go::Context|SPVM::Go::Context>
 
 =head1 Repository
 
@@ -83,5 +114,3 @@ Yuki Kimoto C<kimoto.yuki@gmail.com>
 Copyright (c) 2026 Yuki Kimoto
 
 MIT License
-
-=cut
