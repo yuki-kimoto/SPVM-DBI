0839e41b93427a29eb4ff845a84d24debc224749
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-02-12 08:55:02 +0900

    Add POD documentation for DBI::Data class.

--- lib/SPVM/DBI/Data.pm
+++ lib/SPVM/DBI/Data.pm
@@ -1,63 +1,81 @@
 package SPVM::DBI::Data;
 
-
-
 1;
 
 =encoding utf8
 
 =head1 Name
 
-SPVM::DBI::Data - Short Description
+SPVM::DBI::Data - Data Container for Special Types
 
 =head1 Description
 
-DBI::Data class in L<SPVM> has methods to do someting.
+L<DBI::Data|SPVM::DBI::Data> class in L<SPVM> represents a data container for special types that cannot be represented by basic types like C<int> or C<double>.
+
+This class holds a value as a C<string> and a C<type_id> to indicate how the value should be interpreted (e.g., as a large integer or a high-precision decimal).
 
 =head1 Usage
 
   use DBI::Data;
 
-=head1 Details
-
-
-
-=head1 Super Class
-
-
-
-=head1 Interfaces
-
-
+  # Create a new data container
+  my $data = DBI::Data->new;
+  $data->set_type_id(DBI::Data->TYPE_ID_BIG_INT);
+  $data->set_value("123456789012345678901234567890");
 
 =head1 Enumerations
 
+The following type IDs are defined in the C<enum> section:
 
+=over 4
+
+=item * C<TYPE_ID_UNKNOWN> (0)
+
+The data type is unknown.
+
+=item * C<TYPE_ID_BLOB> (1)
+
+The data is a Binary Large Object (BLOB).
+
+=item * C<TYPE_ID_BIG_INT> (2)
+
+The data is a large integer (e.g., C<Math::BigInt> in Perl).
+
+=item * C<TYPE_ID_BIG_FLOAT> (3)
+
+The data is a high-precision floating-point number (e.g., C<Math::BigFloat> in Perl).
+
+=back
 
 =head1 Fields
 
+=head2 type_id
 
+C<has type_id : rw int;>
+
+An ID that represents the data type. One of the values defined in the L</"Enumerations"> section.
+
+=head2 value
+
+C<has value : rw string;>
+
+The data value represented as a string.
 
 =head1 Class Methods
 
+=head2 new
 
+C<static method new : L<DBI::Data|SPVM::DBI::Data> ()>
 
-=head1 Instance Methods
-
-
-
-=head1 Well Known Child Classes
-
-
+Creates a new L<DBI::Data|SPVM::DBI::Data> object.
 
 =head1 See Also
 
-
-
+L<DBI|SPVM::DBI>, L<DBI::St|SPVM::DBI::St>
 
 =head1 Repository
 
-
+L<https://github.com/yuki-kimoto/SPVM-DBI>
 
 =head1 Author
 
@@ -68,4 +86,3 @@ Yuki Kimoto C<kimoto.yuki@gmail.com>
 Copyright (c) 2026 Yuki Kimoto
 
 MIT License
-
