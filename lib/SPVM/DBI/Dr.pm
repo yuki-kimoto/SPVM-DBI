ce187ce0fda3c6e0e552cbe255344d62e611a39a
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-02-11 16:21:03 +0900

    Improve the document for DBD::Dr.

--- lib/SPVM/DBI/Dr.pm
+++ lib/SPVM/DBI/Dr.pm
@@ -14,6 +14,10 @@ L<DBI::Dr|SPVM::DBI::Dr> class in L<SPVM> represents a driver handle. This class
 
 =head1 Usage
 
+For Driver Authors:
+
+The following example shows how to implement a specific database driver (DBD) by extending the L<DBI::Dr|SPVM::DBI::Dr> class.
+
   class DBD::MyDriver::Dr extends DBI::Dr {
     
     # Overriding the connect method
@@ -21,10 +25,13 @@ L<DBI::Dr|SPVM::DBI::Dr> class in L<SPVM> represents a driver handle. This class
       
       my $dbh = DBD::MyDriver::Db->new;
       
-      # Call the common connection logic provided by the base class
+      # Call the common connection logic provided by the base class.
+      # This validates options and stores them into the database handle ($dbh).
       $self->connect_common($dbh, $ctx, $dsn, $user, $password, $options);
       
-      # Implement the driver-specific logic to connect to a database
+      # Implement the driver-specific logic to connect to a database.
+      # You should apply network-related settings (e.g., TCPNoDelay, ConnectTimeoutDurationNsec) 
+      # to the actual socket here.
       # ...
       
       return $dbh;
@@ -38,15 +45,67 @@ L<DBI::Dr|SPVM::DBI::Dr> class in L<SPVM> represents a driver handle. This class
 
 C<method connect : L<DBI::Db|SPVM::DBI::Db> ($ctx : L<Go::Context|SPVM::Go::Context>, $dsn : string, $user : string = undef, $password : string = undef, $options : object[] = undef)>
 
-In a child class, this method must establish a database connection and return a database handle (L<DBI::Db|SPVM::DBI::Db>).
+Establishes a connection to the database specified by the C<$dsn> and returns a database handle (L<DBI::Db|SPVM::DBI::Db>).
 
-Exceptions:
+The following options can be specified in C<$options>.
 
-Always throws a L<DBI::Error::SQLState|SPVM::DBI::Error::SQLState> exception whose message begins with a 5-character SQLSTATE "IM001" because this method is not implemented.
+=over 4
+
+=item * C<ConnectTimeoutDurationNsec>
+
+The maximum time to wait for the database connection to be established, specified in nanoseconds.
+
+For Driver Authors:
+
+When establishing a connection, you must use this value to set a deadline for the dialer (e.g., C<Go::Net::Dialer>). This ensures that the connection attempt does not hang indefinitely if the database server is unreachable.
+
+=item * C<TCPNoDelay>
+
+A boolean value (0 or 1) to disable Nagle's algorithm. Set to 1 to reduce latency for small packets by sending them immediately.
+
+For Driver Authors:
+
+If this method is not overridden in a child class (a specific DBD), it throws a L<DBI::Error::SQLState|SPVM::DBI::Error::SQLState> exception with SQLSTATE "IM001" (Driver does not support this function) to indicate that the driver implementation is missing.
+
+You must apply this setting to the underlying socket using the C<TCP_NODELAY> option. If this option is not explicitly provided by the user in C<$options>, you should call C<apply_modern_tcp_settings> from L<IO::Socket|SPVM::IO::Socket> to apply optimized defaults.
+
+=item * C<SocketKeepAliveDurationNsec>
+
+The interval for TCP keep-alive probes, specified in nanoseconds. This is useful for maintaining long-lived connections through firewalls or load balancers that might drop idle connections.
+
+For Driver Authors:
+
+You must enable TCP keep-alive on the socket and set the interval using this value. If this option is not explicitly provided by the user, you should call C<apply_modern_tcp_settings> from L<IO::Socket|SPVM::IO::Socket> to ensure reliable default behavior.
+
+=item * C<IdleTimeoutDurationNsec>
+
+The duration a connection can remain idle before it is considered expired and closed by the driver, specified in nanoseconds.
+
+For Driver Authors:
+
+You should use this value to set the C<Timeout> of the L<IO::Socket|SPVM::IO::Socket>. This ensures that the connection is closed if no data is sent or received within the specified duration, preventing resources from being leaked by hung connections.
+
+=item * C<InactiveDestroy>
+
+A boolean value. If set to 1, the C<disconnect> method will not be called automatically when the database handle object is destroyed (C<DESTROY>).
+
+For Driver Authors:
+
+You should set this value to the corresponding field of the database handle object (which should extend L<DBI::Db|SPVM::DBI::Db>). The base class's C<DESTROY> method will automatically check this field to determine whether to call C<disconnect>.
+
+=back
+
+For Driver Authors:
+
+By calling L</"connect_common">, these options are validated and stored in the L<DBI::Db|SPVM::DBI::Db> instance. However, simply storing them in the object is not enough; driver authors are responsible for the actual socket or connection configuration using these stored values.
+
+If this method is not overridden in a child class (a specific DBD), it throws a L<DBI::Error::SQLState|SPVM::DBI::Error::SQLState> exception with SQLSTATE "IM001" (Driver does not support this function) to indicate that the driver implementation is missing.
 
 =head2 connect_common
 
 C<protected method connect_common : void ($dbh : L<DBI::Db|SPVM::DBI::Db>, $ctx : L<Go::Context|SPVM::Go::Context>, $dsn : string, $user : string = undef, $password : string = undef, $options : object[] = undef)>
+
+For Driver Authors:
 
 This method provides common initialization logic for a database handle. It is intended to be called by driver authors within their C<connect> implementation.
 
@@ -95,6 +154,8 @@ By using C<exists> check internally, this method allows driver authors to distin
 =head2 option_names
 
 C<protected method option_names : string[] ()>
+
+For Driver Authors:
 
 Returns an array of supported option names for the database handle.
 
